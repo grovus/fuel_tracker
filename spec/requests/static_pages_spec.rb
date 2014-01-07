31ecbde9d550aaf1eace08c6add0219b7d936924
Sean White <seanywhite@gmail.com>
2014-01-07 13:30:40 -0500

    finish layout and routes plus add initial signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,51 +2,36 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+  	before { visit root_path }
 
-    it "should have the content 'Fuel Tracker'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Fuel Tracker')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Fuel Tracker")
-    end
-
-    it "should not have a custom title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title("| Home")
-    end
+    it { should have_content('Fuel Tracker') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }    
 
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Fuel Tracker | Help")
-    end
-
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Fuel Tracker | About")
-    end
+  describe "Contact page" do
+    before { visit contact_path }
 
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
